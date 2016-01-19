ac651c534063a376f7b6f1875e3cd7ddc2e1a23a
Eric Dill <edill@bnl.gov>
2016-01-19 07:52:30 -0500

    Push to lightsource2, not conda-forge

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -51,7 +51,7 @@ unset LANG
 # state the build dependencies at OS level, too.
 yum install -y libXext libXrender libSM tk libX11-devel
 
-obvci_conda_build_dir /conda-recipes conda-forge --build-condition "numpy >=1.8" "python >=2.7,<3|>=3.4"
+obvci_conda_build_dir /conda-recipes lightsource2 --build-condition "numpy >=1.8" "python >=2.7,<3|>=3.4"
 
 EOF
 
