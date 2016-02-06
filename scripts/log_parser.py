38a333dfa2875428dd707e1a933342c4a1d5f2f9
Eric Dill <edill@bnl.gov>
2016-02-06 17:12:47 -0500

    MNT: Split out the error checking. Document functions

--- scripts/log_parser.py
+++ scripts/log_parser.py
@@ -33,7 +33,7 @@ def read_log_from_script(path_to_log):
             line = line.strip()
             if line.startswith(PACKAGE_NAME_LINE):
                 # split the line on the whitespace that looks something like:
-                # "# $ anaconda upload /tmp/root/ramdisk/mc/conda-bld/linux-64/album-v0.0.2_py35.tar.bz2"
+                # "# $ anaconda upload /some/path/album-v0.0.2_py35.tar.bz2"
                 built_package_path = line.split()[-1]
                 # remove the folder path
                 built_package_name = os.path.split(built_package_path)[-1]
@@ -113,51 +113,113 @@ def parse_conda_build(lines_iterable):
         ret.append((key, bundle))
     return OrderedDict(ret)
 
+def check_for_errors(line, gen):
+    """Check for errors in the generator `gen` that is passed in.
+
+    Specifically look at `line` and see if it starts with "Error: " or
+    "Traceback". If so, start iterating over `gen` until there is a blank line
+    or the generator ends.  Return the last line and any lines that are errors
+
+    Parameters
+    ----------
+    line : str
+        The last line that was taken from `gen`
+    gen : generator
+        The generator that should be iterated over if `line` is the start of
+        an error message
+
+    Returns
+    -------
+    line : str
+        The last line that was taken from `gen`. Might be the same line that
+        was passed in
+    err : list
+        The list of lines that make up the error message. If `err` is an empty
+        list, that signifies that the input `line` is not an error message.
+    """
+    ERROR = "Error: "
+    TRACEBACK = 'Traceback (most recent call last):'
+    err = []
+    if line.startswith(ERROR) or line == TRACEBACK:
+        if line.startswith(ERROR) or line == TRACEBACK:
+            try:
+                while line != '':
+                    err.append(line)
+                    line = next(gen)
+            except StopIteration:
+                # this is thrown when the error goes to the end of the
+                # log section
+                pass
+    return line, err
+
 
 def parse_init(init_section):
+    """Parse the init section from the output of conda build
+
+    Specifically look for the build_command and any errors that may have
+    occurred
+
+    Parameters
+    ----------
+    init_section : iterable
+        Iterable of the lines that occur before conda-build spits out the line
+        that reads: "BUILD START"
+
+    Returns
+    -------
+    dict
+        Keyed on
+        - 'build_command': something like this: conda-build /tmp/staged-recipes/recipes/tifffile --python=3.5
+        - 'err': Might be an empty list. Otherwise it will be a list of lists,
+                 where each top level list will be the output of
+                 `check_for_errors()`
+    """
     ret = {}
-    for line in init_section:
+    gen = (line for line in init_section)
+    ret['err'] = []
+    for line in gen:
         if 'CONDA_CMD' in line:
             ret['build_command'] = line.split('-->')[1].strip()
+        line, err = check_for_errors(line, gen)
+        if err:
+            ret['err'].append(err)
     return ret
 
 
 def parse_build(build_section):
+    """Parse the build section output from `conda-build`. This is everything
+    between the lines that start with "BUILD START" and "BUILD FINISH".
+
+    Parameters
+    ----------
+    build_section : iterable
+        Iterable of all lines between "BUILD START" and "BUILD END"
+
+    Returns
+    -------
+    dict
+        Keyed on
+        - 'built_name': Will either be 'failed' or the full name of the
+                        package that will look something like
+                        suitcase-0.2.1-py35_0
+        - 'err': Might be an empty list. Otherwise it will be a list of lists,
+                 where each top level list will be the output of
+                 `check_for_errors()`
+    """
+    gen = (line for line in build_section)
     PACKAGE_NAME = 'Package: '
-    ERROR = "Error: "
-    TRACEBACK = 'Traceback (most recent call last):'
     ret = {'error': []}
     error = False
     traceback = False
     lines = []
-    for line in build_section:
+    for line in gen:
         if PACKAGE_NAME in line:
+            # format the package name
             ret['built_name'] = line[len(PACKAGE_NAME):]
-        if line.startswith(ERROR) or error:
-            if line == '':
-                error = False
-                ret['error'].append(lines)
-                lines = []
-                ret['built_name'] = 'failed'
-            else:
-                error = True
-                lines.append(line)
-        if line == TRACEBACK or traceback:
-            traceback = True
-            lines.append(line)
+        line, err = check_for_errors(line, gen)
+        if err:
+            ret['error'].append(err)
             ret['built_name'] = 'failed'
-
-        print(line)
-    # the error line might be the last one
-    if error:
-        ret['error'].append(lines)
-        lines = []
-        error = False
-    if traceback:
-        ret['error'].append(lines)
-        lines = []
-        error = False
-        traceback = False
     return ret
 
 
