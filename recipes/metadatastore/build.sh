99afc353dd615d28d093ad9145b053aef232059e
Eric Dill <edill@bnl.gov>
2016-02-16 10:52:30 -0500

    MNT: Fix metadatastore dev recipe

--- recipes/metadatastore/build.sh
+++ recipes/metadatastore/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$PYTHON setup.py build
-$PYTHON setup.py install
+sed -i 's|__CONDA_BUILD_PLACEHOLDER__|'$PREFIX'/etc|' metadatastore/conf.py
+$PYTHON setup.py install --single-version-externally-managed --record=/dev/null
