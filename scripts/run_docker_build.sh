a71092e647034f4d8e206b5cbea8a76d8294a590
Eric Dill <edill@bnl.gov>
2016-04-08 15:59:22 -0400

    MNT: Not sure how libza snuck in there. I'm not sure that is a conda package

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -4,11 +4,11 @@
 
 REPO_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 IMAGE_NAME="ericdill/nsls2-builder:latest"
-
+upload_channel=lightsource2-tag-test
 config=$(cat <<CONDARC
 
 channels:
- - lightsource2-tag-test
+ - $upload_channel
  - conda-forge
  - defaults
 
@@ -61,44 +61,44 @@ echo "
 ===== BUILDING SCIKIT-BEAM RECIPES =====
 
 "
-conda-build-all /tmp/skbeam-recipes/recipes --upload-channels lightsource2-tag-test --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3|>=3.4" --inspect-channels lightsource2-tag-test
+conda-build-all /tmp/skbeam-recipes/recipes --upload-channels $upload_channel --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3|>=3.4" --inspect-channels $upload_channel
 
 echo "
 
 ===== BUILDING NONPY =====
 
 "
-conda-build-all /nonpy-recipes --upload-channels lightsource2-tag-test --inspect-channels lightsource2-tag-test --matrix-conditions "python >=3.5"
+conda-build-all /nonpy-recipes --upload-channels $upload_channel --inspect-channels $upload_channel --matrix-conditions "python >=3.5"
 
 echo "
 
 ===== BUILDING PY2 =====
 
 "
-conda-build-all /py2-recipes --upload-channels lightsource2-tag-test --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3" --inspect-channels lightsource2-tag-test
+conda-build-all /py2-recipes --upload-channels $upload_channel --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3" --inspect-channels $upload_channel
 
 echo "
 
 ===== BUILDING PY2&PY3 =====
 
 "
-conda-build-all /pyall-recipes --upload-channels lightsource2-tag-test --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3|>=3.4" --inspect-channels lightsource2-tag-test
+conda-build-all /pyall-recipes --upload-channels $upload_channel --matrix-conditions "numpy >=1.10,<1.11" "python >=2.7,<3|>=3.4" --inspect-channels $upload_channel
 
 echo "
 
 ===== BUILDING PY3 =====
 
 "
-conda-build-all /py3-recipes --upload-channels lightsource2-tag-test --matrix-conditions "numpy >=1.10,<1.11" "python >=3.4" --inspect-channels lightsource2-tag-test
+conda-build-all /py3-recipes --upload-channels $upload_channel --matrix-conditions "numpy >=1.10,<1.11" "python >=3.4" --inspect-channels $upload_channel
 
 echo "
 
 ===== BUILDING PY3 =====
 
 "
-conda-build-all /py3-recipes --upload-channels lightsource2-tag-test --matrix-conditions "numpy >=1.10,<1.11" "python >=3.4" --inspect-channels lightsource2-tag-test
+conda-build-all /py3-recipes --upload-channels $upload_channel --matrix-conditions "numpy >=1.10,<1.11" "python >=3.4" --inspect-channels $upload_channel
 
 echo "========== Running sort-of-dev-but-actually-tag builds =========="
-devbuild /py3-dev --username lightsource2-tag-test --pyver 3.4 3.5
+devbuild /py3-dev --username $upload_channel --pyver 3.4 3.5
 
 EOF
