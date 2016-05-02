c189d634cd2787c279ef5ea21319e0e4b58cf039
Eric Dill <edill@bnl.gov>
2016-05-02 07:14:38 -0400

    Update build script to use yaml instruction set

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -5,7 +5,7 @@ CLONE_DIR=/tmp/$LOGNAME/staged-recipes-dev
 USERNAME=lightsource2-dev
 
 REPO_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
-IMAGE_NAME="ericdill/nsls2-builder:latest"
+IMAGE_NAME="ericdill/nsls2-conda-builder:latest"
 
 config=$(cat <<CONDARC
 
@@ -43,10 +43,6 @@ echo "$config" > ~/.condarc
 
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
-# conda install "pip<8.1"
-# conda install conda-build conda-build-all --yes
-# conda remove conda-build conda-build-all --yes
-# pip uninstall --yes conda-build conda-build-all
 # pip install https://github.com/conda/conda-build/zipball/master#egg=conda-build
 # git clone https://github.com/ericdill/conda-build
 # cd conda-build-all
@@ -56,14 +52,15 @@ conda clean --lock
 pip install https://github.com/ericdill/conda-build-utils/zipball/master#egg=conda-build-utils
 conda info
 
-# allow failures on the conda-build commands
+# dont allow failures on the conda-build commands
 set -e
-echo "========== Running py2 builds =========="
-devbuild /py2 --username $USERNAME --pyver 2.7 --log $DEV_LOG.summary
-echo "========== Running py3 builds =========="
-devbuild /py3 --username $USERNAME --pyver 3.4 3.5 --log $DEV_LOG.summary
-echo "========== Running pyall builds =========="
-devbuild /pyall --username $USERNAME --pyver 2.7 3.4 3.5 --log $DEV_LOG.summary
+build_from_yaml ../build-directive.yaml -u $USERNAME
+# echo "========== Running py2 builds =========="
+# devbuild /py2 --username $USERNAME --pyver 2.7 --log $DEV_LOG.summary
+# echo "========== Running py3 builds =========="
+# devbuild /py3 --username $USERNAME --pyver 3.4 3.5 --log $DEV_LOG.summary
+# echo "========== Running pyall builds =========="
+# devbuild /pyall --username $USERNAME --pyver 2.7 3.4 3.5 --log $DEV_LOG.summary
 # echo "
 #
 # ===== BUILDING PY2 =====
