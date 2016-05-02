55a4e9870640bd567d15ba094d168e86e881b2a7
Eric Dill <edill@bnl.gov>
2016-05-02 15:26:01 -0400

    Remove old packages

--- pyall/pims-0.2.2/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo $SP_DIR
-
-
-$PYTHON setup.py install --single-version-externally-managed --root /
--- pyall/pyepics-3.2.5rc3/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYEPICS_LIBCA=$PREFIX/lib/libca.so
-echo Using LIBCA from $PYEPICS_LIBCA
-${PYTHON} setup.py build
-${PYTHON} setup.py install
