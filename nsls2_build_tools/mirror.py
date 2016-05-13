b7f31bb7aeda6265b4d1788287b0215b20d28795
Eric Dill <edill@bnl.gov>
2016-05-13 12:21:00 -0400

    Add some documentation

--- nsls2_build_tools/mirror.py
+++ nsls2_build_tools/mirror.py
@@ -114,46 +114,76 @@ def cli():
         pass
     cli = binstar_client.utils.get_server_api(token=args.token, site=args.site)
 
+    # Get the package metadata from the specified anaconda channel
     packages_on_source = cli.show_channel(args.from_label, args.from_owner)
-    files_on_source = [f['full_name'] for f in packages_on_source['files']]
+    files_on_source = [f['basename'] for f in packages_on_source['files']]
     if args.list:
+        # print out the list of all files on the source channel and exit
         print("""
     Listing all files on {}/{}/{}
 """.format(args.site, args.from_owner, args.from_label))
         pprint(files_on_source)
         sys.exit(0)
 
+    # Find the packages on the source channel that match the packages specified
+    # on the command line
     matched = [f for f in files_on_source
                  for p in args.packages if p in f]
+    # and print them out
     print("""
     Packages that match {} on {}/{}/{}
 """.format(args.packages, args.site, args.from_owner, args.from_label))
     pprint(matched)
 
+    # get the package metadata on the target channel
     packages_on_destination = cli.show_channel(args.to_label, args.to_owner)
-    files_on_destination = [f['full_name'] for f in packages_on_destination['files']]
+    files_on_destination = [f['basename'] for f in packages_on_destination['files']]
+    # figure out which of these packages actually need to be copied
     to_copy = [f for f in matched if f not in files_on_destination]
 
+    # print out the packages that need to be copied
     print("""
     Packages that match {} and do not already exist on {}/{}/{}
 """.format(args.packages, args.site, args.to_owner, args.to_label))
     pprint(to_copy)
 
     if args.dry_run:
+        # don't upload anything.  Print out why we are quitting and then quit
         print("""
     Exiting because --dry-run flag is set
 """)
         sys.exit(0)
 
-
-    # spec = '{}/{}/{}'.format(args.from_owner, args.from_label, to_copy[0])
-    for full_name in to_copy:
+    # now grab the full file names, which anaconda upload needs.  This is the
+    # 'spec' portion of the `anaconda upload` command
+    full_names = [f['full_name'] for f in packages_on_source['files']
+                  if f['basename'] in to_copy]
+    print("Actual package names to be uploaded")
+    pprint(full_names)
+    # loop over all packages that need to be copied
+    for full_name in full_names:
         cmd = ['anaconda', 'copy',
                '--to-owner', args.to_owner,
                '--to-label', args.to_label,
                full_name]
-        print(' '.join(cmd))
-        Popen(cmd)
+
+        printable_cmd = cmd
+        if args.token is not None:
+            # insert the token argument only if it exists. Also scrub it
+            # for printing
+            cmd.insert(1, args.token)
+            cmd.insert(1, '--token')
+            printable_cmd = cmd.copy()
+            printable_cmd[2] = 'SCRUBBED_TOKEN'
+        print('Upload command: {}'.format(' '.join(printable_cmd)))
+        # actually do the upload
+        stdout, stderr, returncode = Popen(cmd)
+        # print(stdout)
+        if returncode != 0:
+            print("""
+    stderr from {}
+""".format(cmd))
+            pprint(stderr)
 
 
 if __name__ == "__main__":
