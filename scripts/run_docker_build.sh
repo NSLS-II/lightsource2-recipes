272467ac838ac655e4638a8e26e7a8d15bf6debd
Eric Dill <edill@bnl.gov>
2016-05-18 14:51:43 -0400

    Strip extra '+' in the sed command

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -46,7 +46,7 @@ conda info
 
 # dont allow failures on the conda-build commands
 set -e
-+sed "s/'.'/https:\/\/github.com\/nsls-ii\/auto-build-tagged-recipes/g" /repo/build-directive.yaml -i
+sed "s/'.'/https:\/\/github.com\/nsls-ii\/auto-build-tagged-recipes/g" /repo/build-directive.yaml -i
 build_from_yaml /repo/build-directive.yaml -u $upload_channel
 conda-build-all /repo/x.x --upload-channels $upload_channel --inspect-channels $upload_channel --matrix-conditions "numpy >=1.10" "python >=3.4"
 # These are some standard tools. But they aren't available to a recipe at this point (we need to figure out how a recipe should define OS level deps)
