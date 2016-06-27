12fd6fac68a541e74fa9f2ff732ea964245e6442
Eric Dill <edill@bnl.gov>
2016-06-27 12:00:12 -0400

    Clean up dependency resolution order. Remove emailing from auto build script

--- nsls2_build_tools/build.py
+++ nsls2_build_tools/build.py
@@ -72,7 +72,7 @@ def get_file_names_on_anaconda_channel(username, anaconda_cli,
 
 
 def Popen(cmd):
-    """Returns stdout and stderr
+    """Returns stdout, stderr and the return code
 
     Parameters
     ----------
@@ -251,16 +251,6 @@ def sanitize_names(list_of_names):
     return list_of_names
 
 
-def make_network(packages_dict):
-    G = nx.DiGraph()
-    for package, deps in packages_dict.items():
-        if deps == []:
-            G.add_node(package)
-        for dep in deps:
-            G.add_edge(package, dep)
-    return G
-
-
 def build_dependency_graph(metas):
     """
     Given an input list of MetaData objects, build a directional graph of
@@ -310,7 +300,49 @@ def build_dependency_graph(metas):
     for name, items in union.items():
         union[name] = [item for item in items if item in union]
 
-    return make_network(union)
+    return resolve_dependencies(union)
+
+
+def resolve_dependencies(package_dependencies):
+    """
+    Given a dictionary mapping a package to its dependencies, return a
+    generator of packages to install, sorted by the required install
+    order.
+
+    >>> deps = resolve_dependencies({'a': ['b', 'c'], 'b': ['c'],
+                                     'c': ['d'], 'd': []})
+    >>> list(deps)
+    ['d', 'c', 'b', 'a']
+
+    """
+    remaining_dependencies = package_dependencies.copy()
+    completed_packages = []
+
+    # A maximum of 10000 iterations. Beyond that and there is probably a
+    # problem.
+    for failsafe in range(10000):
+        for package, deps in sorted(remaining_dependencies.copy().items()):
+            if all(dependency in completed_packages for dependency in deps):
+                completed_packages.append(package)
+                remaining_dependencies.pop(package)
+                yield package
+            else:
+                # Put a check in to ensure that all the dependencies were
+                # defined as packages, otherwise we will never succeed.
+                for dependency in deps:
+                    if dependency not in package_dependencies:
+                        msg = ('The package {} depends on {}, but it was not '
+                               'part of the package_dependencies dictionary.'
+                               ''.format(package, dependency))
+                        raise ValueError(msg)
+
+        # Close off the loop if we've completed the dependencies.
+        if not remaining_dependencies:
+            break
+    else:
+        raise ValueError('Dependencies could not be resolved. '
+                         'Remaining dependencies: {}'
+                         ''.format(remaining_dependencies))
 
 
 def run_build(metas, username, token=None, upload=True):
@@ -336,10 +368,10 @@ def run_build(metas, username, token=None, upload=True):
     """
     if token is None:
         token = get_binstar_token()
-    dependency_graph = build_dependency_graph(metas)
+    metas_name_order = build_dependency_graph(metas)
     # pdb.set_trace()
-    print('dependency_graph=%s' % dependency_graph)
-    metas_name_order = nx.topological_sort(dependency_graph)
+    print('dependency_graph=%s' % metas_name_order)
+    # metas_name_order = resolve_dependencies(dependency_graph)
     build_order = [meta for name in metas_name_order for meta in metas
                    if meta.meta['package']['name'] == name]
     # print('metas_order = {}'.format(metas_order))
