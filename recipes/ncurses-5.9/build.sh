14b5942fd878258270ce62ecbde02b7ab871169a
Eric Dill <edill@bnl.gov>
2016-06-28 10:02:19 -0400

    Remove ncurses. It exists on conda-forge

--- recipes/ncurses-5.9/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure --prefix=$PREFIX \
-    --without-debug --without-ada --without-manpages \
-    --with-shared --disable-overwrite --with-termlib=tinfo \
-    --with-normal --enable-widec
-
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
