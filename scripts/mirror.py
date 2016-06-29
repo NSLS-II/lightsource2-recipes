d57ed09b2c33008a67d6225e675ae6ba1f8b4abf
Eric Dill <edill@bnl.gov>
2016-06-29 14:02:39 -0400

    Local updates at nsls2 to get the cloner working

--- scripts/mirror.py
+++ scripts/mirror.py
@@ -14,11 +14,12 @@ from pprint import pprint
 import re
 import sys
 import subprocess
+import tempfile
 
 import binstar_client
 
 
-def Popen(cmd):
+def Popen(cmd, *args, **kwargs):
     """Returns stdout and stderr
 
     Parameters
@@ -31,7 +32,7 @@ def Popen(cmd):
     stdout : """
     # capture the output with subprocess.Popen
     try:
-        proc = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(cmd, stderr=subprocess.PIPE, *args, **kwargs)
     except subprocess.CalledProcessError as cpe:
         print(cpe)
     stdout, stderr = proc.communicate()
@@ -55,6 +56,13 @@ def cli():
         help="List of package names to mirror from one channel to another"
     )
     p.add_argument(
+        '--all',
+        action='store_true',
+        help=("Supercedes `packages` argument. Mirror *all* of the packages "
+              "from `from-owner` to `to-owner`."),
+        default=False
+    )
+    p.add_argument(
         '--list',
         action='store_true',
         help='List all the packages on --from-user and then exit'
@@ -75,10 +83,16 @@ def cli():
     )
     p.add_argument(
         '--from-token',
-        nargs="1",
+        nargs='?',
         help=("anaconda token used to authenticate you to the given anaconda "
               "site. Required for uploading unless you are logged in (via "
               "`anaconda login`)"),
+    )
+    p.add_argument(
+        '--from-disable-verify',
+        action='store_false',
+        help=('ssl verify connection to the `from_site`'),
+        default=True
     )
     p.add_argument(
         '--to-owner',
@@ -96,10 +110,16 @@ def cli():
     )
     p.add_argument(
         '--to-token',
-        nargs="1",
+        nargs='?',
         help=("anaconda token used to authenticate you to the given anaconda "
               "site. Required for uploading unless you are logged in (via "
               "`anaconda login`)"),
+    )
+    p.add_argument(
+        '--to-disable-verify',
+        action='store_false',
+        help=('ssl verify connection to the `from_site`'),
+        default=True
     )
     p.add_argument(
         '--dry-run',
@@ -107,12 +127,33 @@ def cli():
         help=("Figure out which packages would be copied, print it out and "
               "then exit")
     )
+    p.add_argument(
+        '--platform',
+        nargs="*",
+        action="store",
+        help=("Only copy packages for the listed platforms. Options are "
+              "'osx-32', 'osx-64', 'linux-32', 'linux-64', 'win-32' and "
+              "'win-64'.  Defaults to 'linux-64'"),
+        default=["linux-64"]
+    )
 
     args = p.parse_args()
     args.to_channel = 'main'
     args.from_channel = 'main'
-    print(args)
 
+    print("\nSummary"
+          "\n-------")
+    print("Mirroring from", args.from_owner, "at", args.from_domain)
+    print("Mirroring to", args.to_owner, "at", args.to_domain)
+    print("\nPlatforms"
+          "\n---------")
+    pprint(args.platform)
+    print("\nPackages list"
+          "\n-------------")
+    if args.all:
+        print("**all packages**")
+    else:
+        pprint(args.packages)
     try:
         args.from_owner, args.from_channel = args.from_owner.split('/')
     except ValueError:
@@ -124,26 +165,32 @@ def cli():
         # no extra channel information was passed
         pass
     from_cli = binstar_client.Binstar(token=args.from_token,
-                                      domain=args.from_domain)
+                                      domain=args.from_domain,
+                                      verify=args.from_disable_verify)
     to_cli = binstar_client.Binstar(token=args.to_token,
-                                    domain=args.to_domain)
+                                    domain=args.to_domain, 
+                                    verify=args.to_disable_verify)
 
     # Get the package metadata from the specified anaconda channel
     from_packages = from_cli.show_channel(args.from_channel,
-                                               args.from_owner)
-    from_files = [f['basename'] for f in from_packages['files']]
+                                          args.from_owner)
+    from_files = {f['basename']: f for f in from_packages['files'] 
+                  if f['attrs']['subdir'] in args.platform}
     if args.list:
         # print out the list of all files on the source channel and exit
         print("""
     Listing all files on {}/{}/{}
-""".format(args.site, args.from_owner, args.from_channel))
-        pprint(from_files)
+""".format(args.from_domain, args.from_owner, args.from_channel))
+        pprint(list(from_files.keys()))
         sys.exit(0)
 
     # Find the packages on the source channel that match the packages specified
     # on the command line
-    matched = [f for f in from_files
-                 for p in args.packages if p in f]
+    if args.all:
+        matched = list(from_files.keys())
+    else:
+        matched = [f for f in from_files.keys()
+                   for p in args.packages if p in f]
     # and print them out
     print("""
     Packages that match {} on {}/{}/{}
@@ -151,11 +198,10 @@ def cli():
     pprint(matched)
 
     # get the package metadata on the target channel
-    packages_on_destination = to_cli.show_channel(args.to_channel, args.to_owner)
-    files_on_destination = [f['basename']
-                            for f in packages_on_destination['files']]
+    to_packages = to_cli.show_channel(args.to_channel, args.to_owner)
+    to_files = {f['basename']: f for f in to_packages['files']}
     # figure out which of these packages actually need to be copied
-    to_copy = [f for f in matched if f not in files_on_destination]
+    to_copy = [f for f in matched if f not in to_files.keys()]
 
     # print out the packages that need to be copied
     print("""
@@ -169,7 +215,48 @@ def cli():
     Exiting because --dry-run flag is set
 """)
         sys.exit(0)
+    download_dir = tempfile.TemporaryDirectory(prefix='mirror')
 
+    upload_cmd = ['anaconda',
+                  '--site', args.to_domain,
+                  '-t', args.to_token,
+                  'upload', 
+                  '-u', args.to_owner,]
+    for copy_filename in to_copy:
+        # get the full metadata
+        md = from_files[copy_filename]
+        login, package_name, version, platform, filename = md['full_name'].split('/')
+        destination = os.path.join(download_dir.name, filename)
+        print("Downloading {} to {}".format(md['basename'], destination))
+        ret = from_cli.download(login, package_name, md['version'], md['basename'])
+        with open(destination, 'wb') as f:
+            f.write(ret.content)
+        assert os.stat(destination).st_size == md['size']
+    
+        stdout, stderr, returncode = Popen(upload_cmd + [destination], cwd=download_dir.name)
+        if returncode != 0:
+            print("""
+    stderr from {}
+""".format(upload_cmd))
+            pprint(stderr)
+            sys.exit(1)
+    sys.exit(0) 
+    upload_cmd = ['anaconda',
+                  '--site', args.to_domain,
+                  'upload', 
+                  '-u', args.to_owner,
+                  '-t', args.to_token,
+                  '*'] 
+
+    stdout, stderr, returncode = Popen(upload_cmd, cwd=download_dir.name)
+    if returncode != 0:
+        print("""
+    stderr from {}
+""".format(upload_cmd))
+        pprint(stderr)
+        sys.exit(1)
+    sys.exit(0)
+  
     # now grab the full file names, which anaconda upload needs.  This is the
     # 'spec' portion of the `anaconda upload` command
     # download_files =
@@ -201,6 +288,7 @@ def cli():
     stderr from {}
 """.format(cmd))
             pprint(stderr)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
