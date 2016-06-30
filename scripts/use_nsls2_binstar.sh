52762252b9c5dbf09fe1a366bbdba9359be4d2dc
Eric Dill <edill@bnl.gov>
2016-06-29 23:15:13 -0400

    Add scripts to switch between private/public anaconda server

---
+++ scripts/use_nsls2_binstar.sh
@@ -0,0 +1,9 @@
+echo "url: https://anaconda.nsls2.bnl.gov/api" > ~/.config/binstar/config.yaml
+echo "channels:
+- lightsource2-dev
+- lightsource2-tag
+- conda-forge
+- anaconda
+always_yes: true
+ssl_verify: false
+show_channel_urls: true" > ~/.condarc
---
+++ scripts/use_public_anaconda.sh
@@ -0,0 +1,9 @@
+echo "url: https://api.anaconda.org" > ~/.config/binstar/config.yaml
+echo "channels:
+ - lightsource2-dev
+ - lightsource2-tag
+ - conda-forge
+ - defaults
+always_yes: true
+show_channel_urls: true
+binstar_upload: true" > ~/.condarc
