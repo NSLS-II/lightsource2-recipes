ff86bcad72359c4d1f76a46965054f47240ad3d7
Eric Dill <edill@bnl.gov>
2016-06-30 23:29:08 -0400

    Require a slack token for 'mirror-to-nsls2.sh'

--- scripts/mirror-to-nsls2.sh
+++ scripts/mirror-to-nsls2.sh
@@ -4,13 +4,8 @@
 : ${OWNER?"Need to set OWNER"}
 : ${FROM_TOKEN?"Need to set FROM_TOKEN"}
 : ${TO_TOKEN?"Need to set TO_TOKEN"}
+: ${SLACK_TOKEN?"Need to set TO_TOKEN"}
 
-if [ -z ${SLACK_TOKEN+x} ]; then
-  slack_arg="--slack-token $SLACK_TOKEN"
-else
-  slack_arg=""
-fi;
-echo slack_arg=$slack_arg
 # token from nsls2builder on anaconda.org
 from_domain="https://api.anaconda.org"
 
@@ -21,4 +16,4 @@ LOGDIR=~/mirror-logs/`date +%Y`/`date +%m`/`date +%d`
 mkdir -p $LOGDIR
 LOGFILE="$LOGDIR/`date +%H.%M`-mirror-$OWNER"
 
-./mirror.py --from-token $FROM_TOKEN --from-owner $OWNER --from-domain $from_domain --to-token $TO_TOKEN --to-owner $OWNER --to-domain $to_domain --platform linux-64 --to-disable-verify --log $LOGFILE --all $slack_arg
+./mirror.py --from-token $FROM_TOKEN --from-owner $OWNER --from-domain $from_domain --to-token $TO_TOKEN --to-owner $OWNER --to-domain $to_domain --platform linux-64 --to-disable-verify --log $LOGFILE --all --slack-token $SLACK_TOKEN
