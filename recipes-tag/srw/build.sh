1ccb27838af855591ca844d54a2fabfc8e8863f1
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-04-26 15:26:25 -0400

    enable "make all"; fix copying of the built files

--- recipes-tag/srw/build.sh
+++ recipes-tag/srw/build.sh
@@ -1,7 +1,8 @@
-# make all
 d=$(python -c 'from __future__ import print_function; import distutils.sysconfig as s; print(s.get_python_lib())')
+rm -fv $d/*.so
+make all
 (
     cd env/work/srw_python
-    install -m 644 {srwl,uti}*.py srwlpy.so "$d"
+    install -m 644 {srwl,uti}*.py srwlpy*.so "$d"
 )
 
