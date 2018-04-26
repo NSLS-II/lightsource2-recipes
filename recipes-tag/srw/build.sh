5c71991d87e443682d1d86216e58828608f750e7
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-04-26 15:33:00 -0400

    Fix cleaning procedure for .so files

--- recipes-tag/srw/build.sh
+++ recipes-tag/srw/build.sh
@@ -1,8 +1,9 @@
 d=$(python -c 'from __future__ import print_function; import distutils.sysconfig as s; print(s.get_python_lib())')
-rm -fv $d/*.so
+srw_path='env/work/srw_python'
+rm -fv ${srw_path}/*.so
 make all
 (
-    cd env/work/srw_python
+    cd ${srw_path}
     install -m 644 {srwl,uti}*.py srwlpy*.so "$d"
 )
 
