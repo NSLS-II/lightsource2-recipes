6790baab0b3d2291907a264a113dc7d17d68e119
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-04-26 23:37:08 -0400

    SRW: get rid of doubled srwlpy*.so files

--- recipes-tag/srw/build.sh
+++ recipes-tag/srw/build.sh
@@ -1,9 +1,27 @@
 d=$(python -c 'from __future__ import print_function; import distutils.sysconfig as s; print(s.get_python_lib())')
+
 srw_path='env/work/srw_python'
-rm -fv ${srw_path}/*.so
+so_basename='srwlpy'
+
+rm -fv ${srw_path}/${so_basename}*.so
+
+# Get and compile fftw2 lib & compile SRW *.so lib:
 make all
+
 (
     cd ${srw_path}
-    install -m 644 {srwl,uti}*.py srwlpy*.so "$d"
+
+    # This file is generated in Python 2.7:
+    so_file="${so_basename}.so"
+
+    # This file is generated in Python 3.6, but the file above is also generated:
+    cpython_so_file=$(ls -1 ${so_basename}*.so 2>/dev/null | grep -v ${so_file})
+
+    if [ ! -z "${cpython_so_file}" ]; then
+        so=${cpython_so_file}
+    else
+        so=${so_file}
+    fi
+    install -m 644 {srwl,uti}*.py ${so} "$d"
 )
 
