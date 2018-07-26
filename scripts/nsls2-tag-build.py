cc171fccb02db882d2705cae705951265e7a07e6
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-07-26 12:34:11 -0400

    DOC: added a doc string; parametrized more inputs

--- scripts/nsls2-tag-build.py
+++ scripts/nsls2-tag-build.py
@@ -4,7 +4,25 @@ import docker
 from pathlib import Path
 
 
-def run_container(*, pkg_name, pythons=('3.5', '3.6', '3.7')):
+def run_container(*, pkg_name,
+                  docker_image='mrakitin/debian-with-miniconda:latest',
+                  pythons=('3.5', '3.6', '3.7'),
+                  numpy_version='1.14'):
+    """
+    Run a Docker container with supplied commands.
+
+    Parameters
+    ----------
+    pkg_name : str
+        the name of a package to build
+    docker_image : str, optional
+        the name of the docker image to use for building
+    pythons : tuple, optional
+        versions of Python to build the package with
+    numpy_version : str, optional
+        the version of NumPy to build the package with
+    """
+
     # Date-time vars
     start_time = datetime.datetime.now()
     timestamp = datetime.datetime.strftime(start_time, '%Y%m%d%H%M%S')
@@ -16,14 +34,13 @@ def run_container(*, pkg_name, pythons=('3.5', '3.6', '3.7')):
     slack_token = os.getenv('SLACK_TOKEN')
 
     # Docker run options
-    docker_image = 'mrakitin/debian-with-miniconda:latest'
     container_name = f'{pkg_name}-{timestamp}'
     pythons_str = ' '.join(pythons)
     command = ['./repo/scripts/build.py',
                f' /repo/recipes-tag/{pkg_name}',
                f'-u {upload_channel}',
                f'--python {pythons_str}',
-               '--numpy 1.14',
+               f'--numpy {numpy_version}',
                f'--token {binstar_token}',
                f'--slack-channel {slack_channel}',
                f'--slack-token {slack_token}',
@@ -39,12 +56,12 @@ def run_container(*, pkg_name, pythons=('3.5', '3.6', '3.7')):
     # Run Docker container
     docker_client = docker.from_env()
     output = docker_client.containers.run(docker_image,
-                               name=container_name,
-                               command=f'sh -c "{command}"',
-                               remove=True,
-                               stdout=True,
-                               stderr=True,
-                               volumes=volumes).decode()
+                                          name=container_name,
+                                          command=f'sh -c "{command}"',
+                                          remove=True,
+                                          stdout=True,
+                                          stderr=True,
+                                          volumes=volumes).decode()
     end_time = datetime.datetime.now()
     print(f'Output:\n{"=" * 80}\n{output}\n')
     print(f'Duration: {end_time - start_time}')
