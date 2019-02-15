cacb6473a0e266cb05114617a3728cb56cb41375
Maksim Rakitin <mrakitin@users.noreply.github.com>
2019-02-15 14:26:42 -0500

    CLI: added command line interface for the NSLS-II Python building script (#560)

--- scripts/nsls2_tag_build.py
+++ scripts/nsls2_tag_build.py
@@ -1,12 +1,13 @@
-import os
+import argparse
 import datetime
 import docker
+import os
 from pathlib import Path
 
 
 def run_container(*, pkg_name,
-                  pythons=('3.5', '3.6', '3.7'),
-                  numpy_versions=('1.14',), upload=True):
+                  python_versions=('3.6', '3.7'), numpy_versions=('1.14',),
+                  no_upload=False, remove=False, no_detach=False):
     """
     Run a Docker container with supplied commands.
 
@@ -14,10 +15,16 @@ def run_container(*, pkg_name,
     ----------
     pkg_name : str
         the name of a package to build
-    pythons : tuple, optional
-        versions of Python to build the package for
+    python_versions : tuple, optional
+        versions of Python to build the package with
     numpy_versions : tuple, optional
-        versions of NumPy to build the package for
+        versions of NumPy to build the package with
+    no_upload : bool
+        do not upload the built package
+    remove : bool
+        remove the container after execution
+    no_detach : bool
+        do not use a detached mode for execution
     """
 
     # Hard-code it for now to avoid typing it every time.
@@ -37,18 +44,18 @@ def run_container(*, pkg_name,
 
     # Docker run options
     container_name = f'{pkg_name}-{timestamp}'
-    pythons_str = ' '.join(pythons)
+    python_str = ' '.join(python_versions)
     numpy_str = ' '.join(numpy_versions)
     command = ['./repo/scripts/build.py',
                f' /repo/recipes-tag/{pkg_name}',
                f'-u {upload_channel}',
-               f'--python {pythons_str}',
+               f'--python {python_str}',
                f'--numpy {numpy_str}',
                f'--token {binstar_token}',
                f'--slack-channel {slack_channel}',
                f'--slack-token {slack_token}',
                '--allow-failures']
-    if not upload:
+    if no_upload:
         command.append('--no-upload')
 
     command = ' '.join(command)
@@ -62,11 +69,56 @@ def run_container(*, pkg_name,
     # Run Docker container
     docker_client = docker.from_env()
     container = docker_client.containers.run(docker_image,
-                                          name=container_name,
-                                          command=f'sh -c "{command}"',
-                                          remove=False,
-                                          stdout=True,
-                                          stderr=True,
-                                          volumes=volumes,
-                                          detach=True)
+                                             name=container_name,
+                                             command=f'sh -c "{command}"',
+                                             remove=remove,
+                                             stdout=True,
+                                             stderr=True,
+                                             volumes=volumes,
+                                             detach=not no_detach)
     return container
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build a package with docker-py')
+    parser.add_argument('-n', '--pkg-names', dest='pkg_names', nargs='+',
+                        type=str, required=True,
+                        help='a list of names of packages to build')
+    parser.add_argument('--python-versions', nargs='*', type=str,
+                        dest='python_versions', default=['3.6', '3.7'],
+                        help='versions of Python to build the package with')
+    parser.add_argument('--numpy-versions', nargs='*', type=str,
+                        dest='numpy_versions', default=['1.14'],
+                        help='versions of NumPy to build the package with')
+    parser.add_argument('--no-upload', action='store_true',
+                        dest='no_upload',
+                        help='do not upload the built package')
+    parser.add_argument('-r', '--remove', action='store_true',
+                        dest='remove',
+                        help='remove the container after execution')
+    parser.add_argument('--no-detach', action='store_true',
+                        dest='no_detach',
+                        help='do not use a detached mode for execution')
+    
+    args = parser.parse_args()
+
+    pkg_names = args.pkg_names
+    if not pkg_names:
+        parser.print_help()
+        parser.exit()
+
+    kwargs = dict(vars(args))
+    del kwargs['pkg_names']  # we don't want to pass it to run_container()
+
+    print(f'The following packages will be built: {", ".join(pkg_names)}')
+    
+    containers = {}
+    # Submit a building job
+    for pkg_name in pkg_names:
+        kwargs['pkg_name'] = pkg_name
+        containers[pkg_name] = run_container(**kwargs)
+
+    if len(containers) == 1:
+        # Print the log to stdout
+        for line in containers[pkg_names[0]].logs(stream=True):
+            print(line.decode().strip())
