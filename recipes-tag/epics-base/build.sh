a54a376d8d7941d76d8881c5675d23e25a649461
Thomas A Caswell <tcaswell@bnl.gov>
2019-06-11 13:37:51 -0400

    BLD: pull changes from conda-forge epics-base (#701)

--- recipes-tag/epics-base/build.sh
+++ recipes-tag/epics-base/build.sh
@@ -3,21 +3,20 @@ install -d $PREFIX/bin
 install -d $PREFIX/lib
 install -d $PREFIX/epics
 
-make -j$(getconf _NPROCESSORS_ONLN)
+# export EPICS_HOST_ARCH as determined
+EPICS_HOST_ARCH=$(perl src/tools/EpicsHostArch.pl)
+export EPICS_HOST_ARCH
+
+make \
+    CMPLR_PREFIX="${HOST}-" \
+    G++="${CXX}" \
+    GNU_DIR="${BUILD_PREFIX}" \
+    -j ${CPU_COUNT}
 
 EPICS_BASE=$PREFIX/epics
-EPICS_HOST_ARCH=$(startup/EpicsHostArch)
 
 # Copy libraries into $PREFIX/lib
-cp -av $PREFIX/epics/lib/$EPICS_HOST_ARCH/lib*so* $PREFIX/lib 2>/dev/null || : # linux
-cp -av $PREFIX/epics/lib/$EPICS_HOST_ARCH/lib*dylib* $PREFIX/lib 2>/dev/null || :  # osx
-
-# Setup symlinks for utilities
-BINS="caget caput camonitor softIoc caRepeater cainfo"
-cd $PREFIX/bin
-for file in $BINS ; do
-	ln -s ../epics/bin/$EPICS_HOST_ARCH/$file .
-done
+cp -av $PREFIX/epics/lib/$EPICS_HOST_ARCH/lib*${SHLIB_EXT}* $PREFIX/lib
 
 # deal with env export
 mkdir -p $PREFIX/etc/conda/activate.d
--- recipes-tag/epics-base/pre-build.py
+++ recipes-tag/epics-base/pre-build.py
@@ -9,7 +9,7 @@ EPICS_INSTALL_PATH = os.path.join(PREFIX, 'epics')
 EPICS_BIN_PATH = os.path.join(EPICS_INSTALL_PATH, 'bin', EPICS_HOST_ARCH)
 
 # set INSTALLATION_LOCATION in CONFIG_SITE
-open('configure/CONFIG_SITE', 'ab').write('\nINSTALL_LOCATION = $(TOP)/%s\n' % os.path.relpath(EPICS_INSTALL_PATH, os.path.curdir).replace('\\', '/'))
+open('configure/CONFIG_SITE', 'ab').write(('\nINSTALL_LOCATION = $(TOP)/%s\n' % os.path.relpath(EPICS_INSTALL_PATH, os.path.curdir).replace('\\', '/')).encode('utf8'))
 
 # create an epics-base.pth file so that epics bin dir is added to the PATH
 if not os.path.exists(SP_DIR):
