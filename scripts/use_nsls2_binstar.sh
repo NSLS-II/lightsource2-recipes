93b2cd7142d4eb5b52d185be56ecd087e327f846
Maksim Rakitin <mrakitin@users.noreply.github.com>
2019-07-02 22:16:05 -0400

    CLN: clean scripts folder from unused files and scripts (#732)

--- scripts/use_nsls2_binstar.sh
+++
@@ -1,9 +0,0 @@
-echo "url: https://conda.nsls2.bnl.gov/api" > ~/.config/binstar/config.yaml
-echo "channels:
-- nsls2-dev
-- nsls2-tag
-- defaults
-- conda-forge
-always_yes: yes
-ssl_verify: false
-show_channel_urls: true" > ~/.condarc
--- scripts/use_public_anaconda.sh
+++
@@ -1,9 +0,0 @@
-echo "url: https://api.anaconda.org" > ~/.config/binstar/config.yaml
-echo "channels:
- - lightsource2-dev
- - lightsource2-tag
- - conda-forge
- - defaults
-always_yes: false
-show_channel_urls: true
-binstar_upload: true" > ~/.condarc
